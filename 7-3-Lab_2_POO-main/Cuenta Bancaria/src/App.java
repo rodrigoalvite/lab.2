5a6035ea16afcf0d9daefdfda0a73f8545d4f2e3
rodrigoalvite <rodrigo.alvite07@gmail.com>
2026-04-30 13:17:28 -0300

    Refactor App class for clarity and comments

--- 7-3-Lab_2_POO-main/Cuenta Bancaria/src/App.java
+++ 7-3-Lab_2_POO-main/Cuenta Bancaria/src/App.java
@@ -1,12 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Cuenta cuenta1 = new Cuenta();
+        Cuenta cuenta1 = new Cuenta(); //cuenta vacia
 
-        var cuenta2 = new Cuenta("Tiziano", "8.8.8.8", 0.2, 100.5);
+        var cuenta2 = new Cuenta("Tiziano", "8.8.8.8", 0.2, 100.5); // crea cuenta con los datos
 
 
-        cuenta1.setNombreCliente("Rodrigo");
+        cuenta1.setNombreCliente("Rodrigo"); // este seria la cuenta1 que usa un getter
 
 
         System.out.println("La cuenta 2 es " + cuenta2.getNombreCliente());
